e03d8fdafba3ef1e04fe6a7568b38c394460ddc7
Bijoy Sijo <bijoysijo21@gmail.com>
2020-08-31 01:37:47 +0530

    config webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// import the bootstrap javascript module
+import "bootstrap"
+
